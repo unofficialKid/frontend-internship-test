687ded497aeb4b7120c8206815ea912eedf04c06
unofficialKid <ppatrycjaciesla@gmail.com>
2020-06-10 21:43:52 +0200

    task3

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,20 @@ document.getElementById("close").addEventListener("click", function(){
     document.querySelector(".popup").style.display="none"
 })
 
-document.getElementById("btn-submit").addEventListener("click", function(){
+const myForm = document.getElementById("popup-form");
+
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
     var check = document.getElementById("check").checked;
-    if(check==false){      
-    alert("You must accept conditions"); return false;}
-    else return true;
+
+    if(check == true){
+        setTimeout(function(){
+        document.querySelector(".popup").style.display="none";
+        document.getElementById("show-popup-form").style.display="none"
+        document.getElementById("show-thankyou").style.display="flex"
+        }, 3000);
+        return false;
+    }else{
+        alert('You must accept all conditions')
+    }
 })
