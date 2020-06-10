7b66ba4972899fd98f5db2f452c9ea8400a28432
unofficialKid <ppatrycjaciesla@gmail.com>
2020-06-10 15:28:13 +0200

    task2

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,10 @@ document.getElementById("show-popup-form").addEventListener("click", function(){
 document.getElementById("close").addEventListener("click", function(){
     document.querySelector(".popup").style.display="none"
 })
+
+document.getElementById("btn-submit").addEventListener("click", function(){
+    var check = document.getElementById("check").checked;
+    if(check==false){      
+    alert("You must accept conditions"); return false;}
+    else return true;
+})
