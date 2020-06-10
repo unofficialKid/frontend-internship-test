1f680fa151a6c535224c10d8b43578be76499817
unofficialKid <ppatrycjaciesla@gmail.com>
2020-06-10 14:26:56 +0200

    task1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 /* Here goes your JS code */
+document.getElementById("show-popup-form").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+})
+
+document.getElementById("close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display="none"
+})
